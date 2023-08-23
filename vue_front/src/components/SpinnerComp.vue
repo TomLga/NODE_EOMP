5ccf6240cc373296284bc975b8bc3c65e3494c39
Mongy <mongidlanjwa@gmail.com>
2023-08-23 15:21:06 +0200

    day2

---
+++ vue_front/src/components/SpinnerComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden"></span>
+          </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue_front/src/views/HomeView.vue
+++ vue_front/src/views/HomeView.vue
@@ -55,27 +55,34 @@
     </div>
 </div>
 
+<div class="card-group">
+  <div class="card">
+    <img src="https://i.postimg.cc/cJJJLYsm/Noise-Champ.jpg" class="card-img-top" alt="kids watch" style="width: 17rem; margin:auto">
+    <div class="card-body" >
+      <h5 class="card-price">R150</h5>
+      <button class="send_btn" type="button">See more</button>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://i.postimg.cc/V6rLj800/Tempo-Pulse-2-0-Smart-watch.jpg" class="card-img-top" alt="womans watch" style="width: 17rem; margin:auto">
+    <div class="card-body">
+      <h5 class="card-price">R999</h5>
+      <button class="send_btn" type="button">See more</button>
+    </div>
+  </div>
+  <div class="card">
+    <img src="https://i.postimg.cc/d0SVk13K/Volkano-Serene-Series.jpg" class="card-img-top" alt="mans watch" style="width: 17rem; margin:auto">
+    <div class="card-body">
+      <h5 class="card-price">R799</h5>
+      <button class="send_btn" type="button">See more</button>
+    </div>
+  </div>
+</div>
 
 
 
 
-  <div class="container-fluid pt-5 pb-3">
-    <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3">Featured Products</span></h2>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col">
-            <div class="card products-card" v-for="product in products" :key="products.id">
-              <img :src="products.image" class="card-img-top img-fluid" alt="">
-              <div class="card-body">
-                <h5 class="card-title">{{ products.name }}</h5>
-                <a :href="project.url" class="btn btn-primary ">Netlify</a>
-                <a :href="project.url1" class="btn btn-primary">Github</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-   </div>
+  
   
 </template>
 
@@ -97,5 +104,31 @@
  width: 990px;
  margin: auto;
 }
+
+.card-group {
+  display: flex;
+  justify-content: space-between;
+  margin: auto;
+}
+
+.card {
+  margin: 5%;
+  box-shadow: 0px 2px 2px 2px rgb(168, 162, 183);
+
+  
+ 
+  border: none;
+}
+
+.card-img-top {
+
+  width: 100%;
+  height: auto;
+}
+
+.card-body {
+  text-align: center;
+}
+
 
 </style>
