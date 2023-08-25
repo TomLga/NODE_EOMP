406b34fc004b7a85d5e6634cccc9bc33ebd67498
Mongy <mongidlanjwa@gmail.com>
2023-08-25 09:19:44 +0200

    logan changes from pushed

--- vue_front/src/components/UpdateUserComp.vue
+++ vue_front/src/components/UpdateUserComp.vue
@@ -1,51 +1,43 @@
 <template>
-   
-  </template>
-  
-  <script>
-   export default {
-    data() {
-      return {
-        users: [
-         
-         ],
-         newUser: {
-           email: '',
-           password: '',
-        showModal: false, 
-    updateUser() {
-        const apiAddress = '';
-        
-        fetch(apiAddress, {
-          method: 'PUT',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(this.newUser),
-        })
-          .then(response => response.json())
-          .then(data => {
-          
-            this.users.push(data);
-            this.clearNewUserFields();
-          })
-          .catch(error => {
-            console.error('Error updating user:', error);
-          });
-      },
-      clearNewProductFields() {
-        this.newUser.email = '';
-        this.newUser.password = '';
-      },
-      }
-     }
-    }
-};
-
-  </script>
-  
-  <style scoped>
- 
-  </style>
-  
-  
+  <div class="container">
+<!-- Modal -->
+<div class="modal fade" id="editUserModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    </div>
+    <div class="modal-body">
+      <h1>Edit User</h1>
+      <label class="label">Product Name</label>
+      <input type="text">
+      <label class="label">Price</label>
+      <input type="text">
+      <label class="label">Category</label>
+      <input type="text">
+      <label class="label">Quantity</label>
+      <input type="text">
+      <label class="label">Image</label>
+      <input type="text">
+      <button class="btn btn-success mt-3" @click="$event => updateProduct()">Update</button>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      <button type="button" class="btn btn-primary">Save changes</button>
+    </div>
+  </div>
+</div>
+</div>
+  </div>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style scoped>
+.modal-body{
+  display: flex;
+  flex-direction: column;
+}
+</style>
--- vue_front/src/store/index.js
+++ vue_front/src/store/index.js
@@ -36,6 +36,16 @@ export default createStore({
     },
     setMsg(state, msg) {
       state.msg = msg
+    },
+    // logan
+    setProducts(state, value) {
+      state.products = value
+    },
+    setProduct( state, value) {
+      state.product = value
+    },
+    setMessage(state, value){
+      state.message = value
     }
   },
 
@@ -75,7 +85,37 @@ export default createStore({
         } catch (error) {
           throw error;
         }
-      }
+      },
+// logan 
+async fetchProducts(context) {
+  const res = await axios.get(`${cUrl}products`)
+  const {results, err} = await res.data;
+  if(results) {
+    context.commit('setProducts', results)
+  }else {
+    context.commit('setMessage', err)
+  }
+},
+async fetchProduct(context, id) {
+  const res = await axios.get(`${cUrl}product/${id}`)
+  const {results, err} = await res.data;
+  if(results) {
+    context.commit('setProduct', results[0])
+  }else{
+    context.commit('setMessage', err)
+  }
+},
+async addProduct(context, payload) {
+  const res = await axios.post(`${cUrl}products`, payload)
+  const {msg, err} = await res.data;
+  if(msg) {
+    context.commit('setMessage', msg)
+  }else if (err) {
+    console.log(err);
+    context.commit('setMessage', err)
+  }
+}
+
     }
   
 
