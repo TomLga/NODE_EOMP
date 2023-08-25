092e72d0255448b04069a457db393d65e5b3998c
TomLga <semitallbox@gmail.com>
2023-08-25 11:54:58 +0200

    admin style

--- vue_front/src/components/UpdateProductComp.vue
+++ vue_front/src/components/UpdateProductComp.vue
@@ -1,34 +1,59 @@
 <template>
-  <div class="container">
-<!-- Modal -->
-<div class="modal fade" id="editProductModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog">
-  <div class="modal-content">
-    <div class="modal-header">
-      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    </div>
-    <div class="modal-body">
-      <h1>Edit Product</h1>
-      <label class="label">Product Name</label>
-      <input type="text">
-      <label class="label">Price</label>
-      <input type="text">
-      <label class="label">Category</label>
-      <input type="text">
-      <label class="label">Quantity</label>
-      <input type="text">
-      <label class="label">Image</label>
-      <input type="text">
-      <button class="btn btn-success mt-3" @click="$event => updateProduct()">Update</button>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      <button type="button" class="btn btn-primary">Save changes</button>
+  <div >
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addProductModal" data-bs-whatever="@mdo">ADD NEW</button>
+
+
+<div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Adding New User</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">ProdID:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">ProdName:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Price:</label> <br>
+            <input type="number">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Category:</label> 
+            <label for="category">Select: </label>
+            <select id="category" name="category">
+              <option value="men">Men</option>
+              <option value="women">Women</option>
+              <option value="kids">Kids</option>
+            </select>
+        
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Stock on Hand(Qty):</label> <br>
+           <input type="number">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Img:</label> <br>
+         <input type="url">
+          </div>
+          
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Add</button>
+      </div>
     </div>
   </div>
 </div>
-</div>
+
+
   </div>
 </template>
 <script>
@@ -36,8 +61,8 @@
   }
 </script>
 <style scoped>
-.modal-body{
-  display: flex;
-  flex-direction: column;
+.modal-body, .modal-footer{
+ background: #474242;
+ color: white;
 }
 </style>
--- vue_front/src/components/UpdateUserComp.vue
+++ vue_front/src/components/UpdateUserComp.vue
@@ -1,34 +1,55 @@
 <template>
-  <div class="container">
-<!-- Modal -->
-<div class="modal fade" id="editUserModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog">
-  <div class="modal-content">
-    <div class="modal-header">
-      <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-    </div>
-    <div class="modal-body">
-      <h1>Edit User</h1>
-      <label class="label">Product Name</label>
-      <input type="text">
-      <label class="label">Price</label>
-      <input type="number">
-      <label class="label">Category</label>
-      <input type="text">
-      <label class="label">Quantity</label>
-      <input type="number">
-      <label class="label">Image</label>
-      <input type="url">
-      <button class="btn btn-success mt-3" @click="$event => updateProduct()">Update</button>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      <button type="button" class="btn btn-primary">Save changes</button>
+  <div >
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addUserModal" data-bs-whatever="@mdo">ADD NEW</button>
+
+<div class="modal fade" id="addUserModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Adding New User</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">UserID:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="mb-3">
+            <label for="recipient-name" class="col-form-label">Name:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">SurName:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Gender:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Email:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Role:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Profile:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+      </div>
     </div>
   </div>
 </div>
-</div>
+
+
   </div>
 </template>
 <script>
@@ -36,8 +57,8 @@
   }
 </script>
 <style scoped>
-.modal-body{
-  display: flex;
-  flex-direction: column;
+.modal-body, .modal-footer{
+ background: #474242;
+ color: white;
 }
 </style>
